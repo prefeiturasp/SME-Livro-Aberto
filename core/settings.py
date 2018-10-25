0e0acb0ffa07ed864d34ae4ffc24a5b19c89a98a
Israel Teixeira <israelst@gmail.com>
2018-10-25 17:22:51 -0300

    Fix allowed hosts casting

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
