178d312dee6609aa84d96b87573dbb4c58561a67
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-15 20:55:39 -0200

    model SubelementoFromTo added

--- from_to_handler/models.py
+++ from_to_handler/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class FonteDeRecursoFromTo(models.Model):
-    """ Creates groups of Fontes de Recurso. """
+    """ Creates groups of Fontes de Recurso """
     code = models.IntegerField()
     name = models.CharField(max_length=100)
     group_code = models.IntegerField()
     group_name = models.CharField(max_length=100)
+
+
+class SubelementoFromTo(models.Model):
+    """ Adds a friendly name to Subelementos """
+    code = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    new_code = models.IntegerField()
+    new_name = models.CharField(max_length=100)
