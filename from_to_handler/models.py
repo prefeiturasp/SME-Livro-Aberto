32c2c66859493af2de6a5ac3dbb1eb4e6684f87e
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-15 20:20:10 -0200

    model FonteDeRecursoFromTo added

--- from_to_handler/models.py
+++ from_to_handler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class FonteDeRecursoFromTo(models.Model):
+    """ Creates groups of Fontes de Recurso. """
+    code = models.IntegerField()
+    name = models.CharField(max_length=100)
+    group_code = models.IntegerField()
+    group_name = models.CharField(max_length=100)
