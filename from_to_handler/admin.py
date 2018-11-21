814ff6f772a3674469cf051c59b27bf0325573ed
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-21 16:23:02 -0200

    admin: adding list_display to all From-To ModelAdmins

--- from_to_handler/admin.py
+++ from_to_handler/admin.py
@@ -7,23 +7,39 @@ from .models import (Deflator, DotacaoFromTo, GNDFromTo, FonteDeRecursoFromTo,
 @admin.register(Deflator)
 class DeflatorAdmin(admin.ModelAdmin):
     ordering = ('year',)
+    list_display = ('year_str', 'index_number', 'variation_percent')
+
+    def year_str(self, obj):
+        return f'{obj.year.strftime("%Y")}'
+    year_str.short_description = 'Ano'
 
 
 @admin.register(DotacaoFromTo)
 class DotacaoFromToAdmin(admin.ModelAdmin):
     ordering = ('indexer',)
+    list_display = ('indexer', 'group_code', 'group_description',
+                    'subgroup_full_code', 'subgroup_description')
+
+    def subgroup_full_code(self, obj):
+        return f'{obj.subgroup_code} ({obj.group_code}.{obj.subgroup_code})'
+    subgroup_full_code.short_description = 'Código do Subgrupo'
 
 
 @admin.register(GNDFromTo)
 class GNDFromToAdmin(admin.ModelAdmin):
     ordering = ('gnd_code',)
+    list_display = ('gnd_code', 'gnd_description', 'elemento_code',
+                    'elemento_description', 'new_gnd_code',
+                    'new_gnd_description')
 
 
 @admin.register(FonteDeRecursoFromTo)
 class FonteDeRecursoFromToAdmin(admin.ModelAdmin):
     ordering = ('code',)
+    list_display = ('code', 'name', 'group_code', 'group_name')
 
 
 @admin.register(SubelementoFromTo)
 class SubelementoFromToAdmin(admin.ModelAdmin):
     ordering = ('code',)
+    list_display = ('code', 'description', 'new_code', 'new_name')
