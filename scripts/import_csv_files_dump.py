dcad2847c1a0adef1b3a62ab03fdec3dc95c2f38
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-11-23 00:42:08 -0200

    import_csv: created function to create the auxiliary budget_execution models

--- scripts/import_csv_files_dump.py
+++ scripts/import_csv_files_dump.py
@@ -1,16 +1,22 @@
 from typing import NamedTuple
 
+from django.apps import apps
 from openpyxl import load_workbook
+
+
+class OtherColumn(NamedTuple):
+    name: str
+    column: str
 
 
 class OrcamentoColumns(NamedTuple):
     id: str
     description: str
-    other_col: dict = None
+    other_col: OtherColumn = None
 
 
 MODELS = {
-    "Orgao": OrcamentoColumns('H', 'J', {'initials': 'I'}),
+    "Orgao": OrcamentoColumns('H', 'J', OtherColumn('initials', 'I')),
     # "ProjetoAtividade": OrcamentoColumns('U', 'V', {'type': 'S'}),
     "Categoria": OrcamentoColumns('Y', 'Z'),
 }
@@ -19,16 +25,26 @@ MODELS = {
 def import_fk_object(ws, row, name):
     id_col = MODELS[name].id
     desc_col = MODELS[name].description
-    # other_col_dict = MODELS[name].other_col
+    other_col_tuple = MODELS[name].other_col
 
-    import ipdb
-    ipdb.set_trace()
+    # import ipdb
+    # ipdb.set_trace()
     row = str(row)
     id = int(ws[id_col + row].value)
     description = ws[desc_col + row].value
-    # other_col =
 
-    # save object
+    Model = apps.get_model('budget_execution', name)
+    obj = Model()
+    obj.id = id
+    obj.description = description.strip()
+
+    if other_col_tuple:
+        other_col = other_col_tuple.column
+        other_col_name = other_col_tuple.name
+        other_col_value = ws[other_col + row].value
+        setattr(obj, other_col_name, other_col_value.strip())
+
+    # obj.save()
 
     return id
 
