b7a0c14081301383c6c35671553a81756e59dbfb
Israel Teixeira <israelst@gmail.com>
2018-11-26 16:58:10 -0200

    Temporary redirect to mosaico home

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='mosaico/')),
     path('mosaico/', include('mosaico.urls')),
 ]
