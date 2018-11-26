8895479a5f472f9667aebd8828910123609dd8e1
Israel Teixeira <israelst@gmail.com>
2018-11-26 15:34:14 -0200

    Route mosaico home and fix static urls

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'mosaico',
     'from_to_handler.apps.FromToHandlerConfig',
 ]
 
