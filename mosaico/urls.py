9804235f28eb8ce18c27936aa17fb1cb9faf0012
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-12-04 01:56:33 -0200

    mosaico: GrupoList view created
    missing tests

--- mosaico/urls.py
+++ mosaico/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from mosaico.views import (
+    GrupoList,
+)
+
 
 app_name = 'mosaico'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='mosaico/base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='mosaico/base.html'),
+         name='home'),
+    path('treemap/<int:year>/', GrupoList.as_view(), name='grupos'),
 ]
--- mosaico/views.py
+++ mosaico/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from rest_framework import generics
+
+from budget_execution.models import Execucao
+from mosaico.serializers import GrupoSerializer
+
+
+class GrupoList(generics.ListAPIView):
+    serializer_class = GrupoSerializer
+
+    def get_queryset(self):
+        year = self.kwargs['year']
+        return Execucao.objects.filter(year=date(year, 1, 1)) \
+            .distinct('subgrupo__grupo')
