8c302069e718a1334411ee31e4b7c7c64724c978
Israel Teixeira <israelst@gmail.com>
2018-12-06 12:15:41 -0200

    Extract base template

--- core/settings/base.py
+++ core/settings/base.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
