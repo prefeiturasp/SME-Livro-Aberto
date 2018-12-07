259f29c0c8f2f3a0ed1b76d7251a05a26ea3bb88
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-12-07 00:18:15 -0200

    geologia: view returning json with summed values

---
+++ geologia/services.py
@@ -0,0 +1,58 @@
+from django.db.models import Sum
+
+
+def prepare_gnd_data(queryset):
+    years = queryset.values('year').distinct()
+
+    ret = {
+        'orcado': [],
+        'empenhado': [],
+    }
+    for year_dict in years:
+        year = year_dict['year']
+        year_qs = queryset.filter(year=year)
+
+        ret['orcado'].append(get_gnd_orcado_data(year_qs))
+        ret['empenhado'].append(get_gnd_empenhado_data(year_qs))
+
+    return ret
+
+
+def get_gnd_orcado_data(queryset):
+    year = queryset[0].year
+
+    orcado_by_gnd = queryset.values('gnd_gealogia__desc') \
+        .annotate(orcado=Sum('orcado_atualizado'))
+    orcado_total = queryset.aggregate(total=Sum('orcado_atualizado'))
+
+    orcado_gnds = [
+        {"name": gnd['gnd_gealogia__desc'],
+         "value": gnd['orcado']}
+        for gnd in orcado_by_gnd
+    ]
+
+    return {
+        "year": year.strftime("%Y"),
+        "total": orcado_total['total'],
+        "gnds": orcado_gnds,
+    }
+
+
+def get_gnd_empenhado_data(queryset):
+    year = queryset[0].year
+
+    empenhado_by_gnd = queryset.values('gnd_gealogia__desc') \
+        .annotate(empenhado=Sum('empenhado_liquido'))
+    empenhado_total = queryset.aggregate(total=Sum('empenhado_liquido'))
+
+    empenhado_gnds = [
+        {"name": gnd['gnd_gealogia__desc'],
+         "value": gnd['empenhado']}
+        for gnd in empenhado_by_gnd
+    ]
+
+    return {
+        "year": year.strftime("%Y"),
+        "total": empenhado_total['total'],
+        "gnds": empenhado_gnds,
+    }
--- geologia/views.py
+++ geologia/views.py
@@ -1,12 +1,19 @@
-from django.db.models import Sum
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework.response import Response
 
 from budget_execution.models import Execucao
+from geologia import services
 
 
 class GeologiaView(generics.ListAPIView):
     renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
     template_name = 'geologia/base.html'
     queryset = Execucao.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        qs = self.get_queryset()
+
+        gnd_data = services.prepare_gnd_data(qs)
+
+        return Response({'gnd': gnd_data})
