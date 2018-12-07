8cac7b572e6c01ab2a496f32ce56edb637645466
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-12-07 00:02:15 -0200

    geologia: first version GeologiaView

--- geologia/views.py
+++ geologia/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework.response import Response
 
-# Create your views here.
+from budget_execution.models import Execucao
+
+
+class GeologiaView(generics.ListAPIView):
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    template_name = 'geologia/base.html'
+    queryset = Execucao.objects.all()
