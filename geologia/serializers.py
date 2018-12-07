f77e6e61959c7b19045e980da9fab1948a2eef7b
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-12-07 16:57:16 -0200

    geologia: serializer returning subfuncao orcado data

--- geologia/serializers.py
+++ geologia/serializers.py
@@ -9,9 +9,11 @@ class GeologiaSerializer:
     @property
     def data(self):
         return {
-            'gnd': self.prepare_gnd_data()
+            'gnd': self.prepare_gnd_data(),
+            'subfuncao': self.prepare_subfuncao_data(),
         }
 
+    # Chart 1: Gnd
     def prepare_gnd_data(self):
         qs = self.queryset
 
@@ -57,6 +59,73 @@ class GeologiaSerializer:
 
         return {
             "year": year.strftime("%Y"),
+            "total": empenhado_total['total'],
+            "gnds": empenhado_gnds,
+        }
+
+    # Chart 3: Subfuncao
+    def prepare_subfuncao_data(self):
+        qs = self.queryset
+
+        years = qs.values('year').distinct()
+
+        ret = {
+            'orcado': [],
+            'empenhado': [],
+        }
+        for year_dict in years:
+            year = year_dict['year']
+            year_qs = qs.filter(year=year)
+
+            ret['orcado'].append(self.get_subfuncao_year_orcado_data(year_qs))
+            # TODO: fix empenhado
+            ret['empenhado'].append(self.get_subfuncao_empenhado_data(year_qs))
+
+        return ret
+
+    def get_subfuncao_year_orcado_data(self, qs):
+        year = qs[0].year
+        subfuncoes = qs.distinct('subfuncao').values('subfuncao_id')
+
+        ret = {
+            'year': year.strftime('%Y'),
+            'subfuncoes': [],
+        }
+
+        for subfuncao in subfuncoes:
+            qs_subfuncao = qs.filter(subfuncao=subfuncao['subfuncao_id'])
+            ret['subfuncoes'].append(
+                self.get_subfuncao_orcado_data(qs_subfuncao))
+
+        return ret
+
+    def get_subfuncao_orcado_data(self, qs):
+        subfuncao = qs[0].subfuncao
+
+        orcado_by_gnd = qs.values('gnd_gealogia__desc') \
+            .annotate(orcado=Sum('orcado_atualizado'))
+        orcado_total = qs.aggregate(total=Sum('orcado_atualizado'))
+
+        orcado_gnds = self._get_orcado_gnds_list(orcado_by_gnd, orcado_total)
+
+        return {
+            "subfuncao": subfuncao.desc,
+            "total": orcado_total['total'],
+            "gnds": orcado_gnds,
+        }
+
+    def get_subfuncao_empenhado_data(self, qs):
+        subfuncao = qs[0].subfuncao
+
+        empenhado_by_gnd = qs.values('gnd_gealogia__desc') \
+            .annotate(empenhado=Sum('empenhado_liquido'))
+        empenhado_total = qs.aggregate(total=Sum('empenhado_liquido'))
+
+        empenhado_gnds = self._get_empenhado_gnds_list(empenhado_by_gnd,
+                                                       empenhado_total)
+
+        return {
+            "subfuncao": subfuncao.desc,
             "total": empenhado_total['total'],
             "gnds": empenhado_gnds,
         }
