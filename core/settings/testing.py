0ba96e5d1d57848ea32e726e986bc35a0748ba00
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-12-17 22:05:55 -0200

    using postgres as test database backend

--- core/settings/testing.py
+++ core/settings/testing.py
@@ -1,14 +1,6 @@
 from .base import *
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
-
-
 MIGRATION_MODULES = {
     'from_to_handler': None,
     'budget_execution': None,
