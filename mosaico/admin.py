fac9297109e1a814118cc0266795f8b3c23dba18
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-01-14 22:54:41 -0200

    mosaico: MinimoLegalSpreadsheetModel added to admin

--- mosaico/admin.py
+++ mosaico/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MinimoLegalSpreadsheetModel
+
+
+@admin.register(MinimoLegalSpreadsheetModel)
+class MinimoLegalSpreadsheetAdmin(admin.ModelAdmin):
+
+    def get_changeform_initial_data(self, request):
+        return {
+            'title_25percent': 'DESPESAS COM AÇÕES TÍPICAS DE MDE',
+            'limit_25percent': 'TOTAL DAS DESPESAS COM AÇÕES TÍPICAS DE MDE',
+            'title_6percent': ('DESPESAS COM AÇÕES TÍPICAS DO PROGRAMA DE '
+                               'EDUCAÇÃO INCLUSIVA'),
+            'limit_6percent': (
+                '60-TOTAL COM DESPESAS COM AÇÕES TÍPICAS DO '
+                'PROGRAMA DE EDUCAÇÃO INCLUSIVA (53+54+55+56+57+58+59)'
+            ),
+        }
