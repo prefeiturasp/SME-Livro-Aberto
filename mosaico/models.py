fd925233db860455af467d78939faabfae0550aa
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-01-15 12:37:36 -0200

    MinimoLegalSpreadsheet: year and created_at field added
    migrations reset

--- mosaico/models.py
+++ mosaico/models.py
@@ -1,9 +1,16 @@
+from datetime import date
+
 from django.db import models
+
+from mosaico import services
 
 
 class MinimoLegalSpreadsheetModel(models.Model):
+    YEAR_CHOICES = [(y, y) for y in range(2018, date.today().year + 1)]
+
     spreadsheet = models.FileField(
         'Planilha', upload_to='mosaico/minimo_legal_spreadsheets')
+    year = models.IntegerField('Ano dos dados', choices=YEAR_CHOICES)
     title_25percent = models.CharField('Título do grupo de dados - 25%',
                                        max_length=200)
     limit_25percent = models.CharField('Texto limite do grupo de dados - 25%',
@@ -12,6 +19,7 @@ class MinimoLegalSpreadsheetModel(models.Model):
                                       max_length=200)
     limit_6percent = models.CharField('Texto limite do grupo de dados - 6%',
                                       max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Dados do Mínimo Legal (31%)'
