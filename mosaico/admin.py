21147ab3a87736c99cdef8d3852d456856095008
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-02-04 12:43:05 -0200

    MinimoLegalSpreadsheet importer: updating spreadsheet format

--- mosaico/admin.py
+++ mosaico/admin.py
@@ -12,8 +12,7 @@ class MinimoLegalSpreadsheetAdmin(admin.ModelAdmin):
             'limit_25percent': (
                 '28- TOTAL DAS DESPESAS COM AÇÕES TÍPICAS DE MDE '
                 '(22+23 + 24 + 25 + 26 + 27)'),
-            'title_6percent': ('DESPESAS COM AÇÕES TÍPICAS DO PROGRAMA DE '
-                               'EDUCAÇÃO INCLUSIVA'),
+            'title_6percent': ('DESPESAS COM EDUCAÇÃO INCLUSIVA'),
             'limit_6percent': (
                 '60-TOTAL COM DESPESAS COM AÇÕES TÍPICAS DO '
                 'PROGRAMA DE EDUCAÇÃO INCLUSIVA (53+54+55+56+57+58+59)'
