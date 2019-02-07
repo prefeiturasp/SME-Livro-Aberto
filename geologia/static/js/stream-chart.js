95d4e586b21e1c904296575929acd6f731874610
Israel Teixeira <israelst@gmail.com>
2019-02-07 12:13:05 -0200

    Use table data to plot stream chart

--- geologia/static/js/stream-chart.js
+++ geologia/static/js/stream-chart.js
@@ -1,19 +1,25 @@
-// Inspired by Lee Byron’s test data generator.
-function bump(a, n) {
-    const x = 1 / (0.1 + Math.random());
-    const y = 2 * Math.random() - 0.5;
-    const z = 10 / (0.1 + Math.random());
-    for (let i = 0; i < n; ++i) {
-        const w = (i / n - y) * z;
-        a[i] += x * Math.exp(-w * w);
-    }
+function year(tr){
+    return parseInt(tr.querySelector('[data-year]').dataset.year);
 }
 
-function bumps(n, m) {
-    const a = [];
-    for (let i = 0; i < n; ++i) a[i] = 0;
-    for (let i = 0; i < m; ++i) bump(a, n);
-    return a;
+function value(tr){
+    return parseFloat(tr.querySelector('[data-value]').dataset.value);
+}
+
+function name(tr){
+    return tr.querySelector('[data-name]').dataset.name;
+}
+
+function item(tr){
+    return {'year': year(tr), 'value': value(tr), 'name': name(tr)};
+}
+
+function emptyObj(keys){
+    let obj = {};
+    keys.forEach(key => {
+        obj[key] = 0;
+    });
+    return obj;
 }
 
 window.addEventListener('load', function(){
@@ -23,23 +29,34 @@ window.addEventListener('load', function(){
     let parentWidth = parseInt(getComputedStyle(svg.node())['width']);
     var margin = {top: 0, right: 0, bottom: 40, left: 0},
     width = parentWidth - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom;
     svg.attr('height', height + margin.top + margin.bottom);
+    let grds = ['contributions', 'operational', 'previous', 'consulting',
+              'realty', 'construction', 'people', 'outsourced'];
 
-    let n = 8;
-    let m = 15;
-    let k = 10;
-    let data = d3.transpose(Array.from({length: n}, () => bumps(m, k)));
+    let rows = document.querySelectorAll('.stream-chart tbody tr');
 
     stack = d3.stack()
-        .keys(d3.range(n))
+        .keys(grds)
         .offset(d3['stackOffsetExpand'])
 
-    const x = d3.scaleLinear()
-        .domain([0, m - 1])
-        .range([0, width]);
+    let grouped = Array.from(rows, item).reduce(function(accumulator, curr){
+        accumulator[curr.year] = accumulator[curr.year] || emptyObj(grds);
+        accumulator[curr.year][curr.name] = curr.value;
+        return accumulator;
+    }, {});
 
+    let data = []
+    let years = []
+    for(key in grouped){
+        years.push(key)
+        data.push(grouped[key])
+    }
     const layers = stack(data)
+
+    const x = d3.scaleLinear()
+        .domain(d3.extent(years))
+        .range([0, width]);
 
     const y = d3.scaleLinear()
         .range([height, 0])
@@ -48,14 +65,9 @@ window.addEventListener('load', function(){
             d3.max(layers, l => d3.max(l, d => d[1]))
         ]);
 
-    const z = d3.scaleOrdinal()
-          .domain(d3.range(n))
-          .range(['contributions', 'operational', 'previous', 'consulting',
-              'realty', 'construction', 'people', 'outsourced', ]);
-
     const area = d3.area()
         .curve(d3.curveMonotoneX)
-        .x((d, i) => x(i))
+        .x((d, i) => x(i + x.domain()[0]))
         .y0(d => y(d[0]))
         .y1(d => y(d[1]));
 
@@ -63,7 +75,7 @@ window.addEventListener('load', function(){
       .data(layers)
       .enter().append('path')
         .attr('d', area)
-        .attr('class', (d, i) => z(i));
+        .attr('class', d => d.key);
 
     table.style.display = 'none';
 })
