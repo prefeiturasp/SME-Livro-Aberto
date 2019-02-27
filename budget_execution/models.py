3eeb3a21154142dd6b9502bd45159e23b40329fa
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-02-27 15:28:14 -0300

    OrcamentoRaw and EmpenhoRaw models created

--- budget_execution/models.py
+++ budget_execution/models.py
@@ -448,7 +448,7 @@ class Orcamento(models.Model):
 
 
 class Empenho(models.Model):
-    """SME dw_orcamento table replica"""
+    """SME dw_empenhos table replica"""
     id = models.IntegerField(primary_key=True)
     cd_key = models.TextField(blank=True, null=True)
     an_empenho = models.BigIntegerField(blank=True, null=True)
@@ -500,6 +500,131 @@ class Empenho(models.Model):
 
     class Meta:
         db_table = 'empenhos'
+
+    @property
+    def indexer(self):
+        s = self
+        return (
+            f'{s.an_empenho}.{s.cd_orgao}.{s.cd_projeto_atividade}.'
+            f'{s.cd_categoria}.{s.cd_grupo}.{s.cd_modalidade}.'
+            f'{s.cd_elemento}.{s.cd_fonte_de_recurso}.{s.cd_subelemento}')
+
+
+class OrcamentoRaw(models.Model):
+    """SME raw_orcamento table replica"""
+    id = models.IntegerField(primary_key=True)
+    cd_key = models.TextField(blank=True, null=True)
+    dt_inicial = models.DateTimeField(blank=True, null=True)
+    dt_final = models.DateTimeField(blank=True, null=True)
+    cd_ano_execucao = models.BigIntegerField(blank=True, null=True)
+    cd_exercicio = models.BigIntegerField(blank=True, null=True)
+    nm_administracao = models.TextField(blank=True, null=True)
+    cd_exercicio_empresa_id = models.BigIntegerField(blank=True, null=True)
+    cd_orgao = models.BigIntegerField(blank=True, null=True)
+    sg_orgao = models.TextField(blank=True, null=True)
+    ds_orgao = models.TextField(blank=True, null=True)
+    cd_unidade = models.BigIntegerField(blank=True, null=True)
+    ds_unidade = models.TextField(blank=True, null=True)
+    cd_funcao = models.BigIntegerField(blank=True, null=True)
+    ds_funcao = models.TextField(blank=True, null=True)
+    cd_subfuncao = models.BigIntegerField(blank=True, null=True)
+    ds_subfuncao = models.TextField(blank=True, null=True)
+    cd_programa = models.BigIntegerField(blank=True, null=True)
+    ds_programa = models.TextField(blank=True, null=True)
+    tp_projeto_atividade = models.TextField(blank=True, null=True)
+    tp_papa = models.TextField(blank=True, null=True)
+    cd_projeto_atividade = models.BigIntegerField(blank=True, null=True)
+    ds_projeto_atividade = models.TextField(blank=True, null=True)
+    cd_despesa = models.BigIntegerField(blank=True, null=True)
+    ds_despesa = models.TextField(blank=True, null=True)
+    ds_categoria_despesa = models.BigIntegerField(blank=True, null=True)
+    ds_categoria = models.TextField(blank=True, null=True)
+    cd_grupo_despesa = models.BigIntegerField(blank=True, null=True)
+    ds_grupo_despesa = models.TextField(blank=True, null=True)
+    cd_modalidade = models.BigIntegerField(blank=True, null=True)
+    ds_modalidade = models.TextField(blank=True, null=True)
+    cd_elemento = models.BigIntegerField(blank=True, null=True)
+    cd_fonte = models.BigIntegerField(blank=True, null=True)
+    ds_fonte = models.TextField(blank=True, null=True)
+    vl_orcado_inicial = models.FloatField(blank=True, null=True)
+    vl_orcado_atualizado = models.FloatField(blank=True, null=True)
+    vl_congelado = models.FloatField(blank=True, null=True)
+    vl_orcado_disponivel = models.FloatField(blank=True, null=True)
+    vl_reservado_liquido = models.FloatField(blank=True, null=True)
+    vl_empenhado_liquido = models.FloatField(blank=True, null=True)
+    vl_empenhado_liquido_atual = models.FloatField(blank=True, null=True)
+    vl_liquidado = models.FloatField(blank=True, null=True)
+    vl_liquidado_atual = models.FloatField(blank=True, null=True)
+    vl_pago = models.FloatField(blank=True, null=True)
+    vl_pago_atual = models.FloatField(blank=True, null=True)
+    vl_saldo_empenho = models.FloatField(blank=True, null=True)
+    vl_saldo_reserva = models.FloatField(blank=True, null=True)
+    vl_saldo_dotacao = models.FloatField(blank=True, null=True)
+    dt_extracao = models.DateTimeField(blank=True, null=True)
+    dt_data_loaded = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'orcamento_raw_load'
+        index_together = ['cd_ano_execucao', 'cd_projeto_atividade']
+
+    @property
+    def indexer(self):
+        s = self
+        return (
+            f'{s.cd_ano_execucao}.{s.cd_orgao}.{s.cd_projeto_atividade}.'
+            f'{s.ds_categoria_despesa}.{s.cd_grupo_despesa}.{s.cd_modalidade}.'
+            f'{s.cd_elemento}.{s.cd_fonte}')
+
+
+class EmpenhoRaw(models.Model):
+    """SME raw_empenhos table replica"""
+    id = models.IntegerField(primary_key=True)
+    cd_key = models.TextField(blank=True, null=True)
+    an_empenho = models.BigIntegerField(blank=True, null=True)
+    cd_categoria = models.BigIntegerField(blank=True, null=True)
+    cd_elemento = models.TextField(blank=True, null=True)
+    cd_empenho = models.BigIntegerField(blank=True, null=True)
+    cd_empresa = models.TextField(blank=True, null=True)
+    cd_fonte_de_recurso = models.TextField(blank=True, null=True)
+    cd_funcao = models.TextField(blank=True, null=True)
+    cd_grupo = models.BigIntegerField(blank=True, null=True)
+    cd_item_despesa = models.TextField(blank=True, null=True)
+    cd_modalidade = models.BigIntegerField(blank=True, null=True)
+    cd_orgao = models.TextField(blank=True, null=True)
+    cd_programa = models.TextField(blank=True, null=True)
+    cd_projeto_atividade = models.TextField(blank=True, null=True)
+    cd_subelemento = models.TextField(blank=True, null=True)
+    cd_subfuncao = models.TextField(blank=True, null=True)
+    cd_unidade = models.TextField(blank=True, null=True)
+    dt_empenho = models.DateTimeField(blank=True, null=True)
+    mes_empenho = models.BigIntegerField(blank=True, null=True)
+    nm_empresa = models.TextField(blank=True, null=True)
+    dc_cpf_cnpj = models.TextField(blank=True, null=True)
+    cd_reserva = models.BigIntegerField(blank=True, null=True)
+    dc_categoria_economica = models.TextField(blank=True, null=True)
+    dc_elemento = models.TextField(blank=True, null=True)
+    dc_fonte_de_recurso = models.TextField(blank=True, null=True)
+    dc_funcao = models.TextField(blank=True, null=True)
+    dc_item_despesa = models.TextField(blank=True, null=True)
+    dc_orgao = models.TextField(blank=True, null=True)
+    dc_programa = models.TextField(blank=True, null=True)
+    dc_projeto_atividade = models.TextField(blank=True, null=True)
+    dc_subelemento = models.TextField(blank=True, null=True)
+    dc_subfuncao = models.TextField(blank=True, null=True)
+    dc_unidade = models.TextField(blank=True, null=True)
+    dc_grupo_despesa = models.TextField(blank=True, null=True)
+    dc_modalidade = models.TextField(blank=True, null=True)
+    dc_razao_social = models.TextField(blank=True, null=True)
+    vl_empenho_anulado = models.FloatField(blank=True, null=True)
+    vl_empenho_liquido = models.FloatField(blank=True, null=True)
+    vl_liquidado = models.FloatField(blank=True, null=True)
+    vl_pago = models.FloatField(blank=True, null=True)
+    vl_pago_restos = models.FloatField(blank=True, null=True)
+    vl_empenhado = models.FloatField(blank=True, null=True)
+    dt_data_loaded = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'empenhos_raw_load'
 
     @property
     def indexer(self):
