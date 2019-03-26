1c92926b4094e18eed45904c2d0e6fee449d6b97
Israel Teixeira <israelst@gmail.com>
2019-03-26 20:19:33 -0300

    Add interaction to both bar charts

--- geologia/static/js/bar-chart.js
+++ geologia/static/js/bar-chart.js
@@ -1,15 +1,19 @@
 window.addEventListener('load', function(){
-    let barchart = d3.select('table.bar-chart');
-    let container = d3.select(barchart.node().parentNode);
+    let barchart = d3.selectAll('table.bar-chart');
     barchart.selectAll('tr')
-      .style('cursor', 'pointer')
-      .on('click', function(){
-        const year = this.dataset.year;
-        if(!year) return;
-        container.select(`.card.active`).classed('active', false);
-        container.select(`.card[data-year="${year}"]`).classed('active', true);
-      })
-    d3.selectAll('table.bar-chart a').on('click', function(){
-        d3.event.preventDefault();
-    });
+        .style('cursor', 'pointer')
+        .on('click', function(){
+            d3.select(this).select('a').dispatch('click');
+        });
+    barchart.selectAll('a')
+        .on('click', function(){
+            const target = new URL(this.href)
+            const selection = d3.select(this.hash);
+            d3.select(selection.node().parentNode)
+                .select('.card.active')
+                .classed('active', false);
+            selection.classed('active', true);
+            d3.event.preventDefault();
+            d3.event.stopPropagation();
+        });
 })
