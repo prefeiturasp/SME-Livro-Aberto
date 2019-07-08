407b8661508c0302db23bc2623b62370343f8429
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-07-08 09:21:21 -0300

    execucoes scripts updated

--- scripts/generate_execucoes.py
+++ scripts/generate_execucoes.py
@@ -4,18 +4,23 @@ from budget_execution import services
 def run(*args):
     load_everything = bool("load_everything" in args)
 
-    services.erase_current_year_data()
+    services.erase_data_to_be_updated(load_everything)
 
     services.load_data_from_orcamento_raw(load_everything)
     print("Data loaded from orcamento_raw_load")
     services.load_data_from_empenhos_raw(load_everything)
     print("Data loaded from empenhos_raw_load")
 
-    services.import_orcamentos(load_everything)
-    print("Orcamento imported")
-    services.import_empenhos(load_everything)
-    print("Empenhos imported")
+    print("Generating execucoes:")
+    print("Importing Minimo Legal")
     services.import_minimo_legal()
-    print("Minimo Legal imported")
+
+    print("Importing orcamentos to ExecucaoTemp")
+    services.import_orcamentos(load_everything)
+    print("Moving execucoes from ExecucaoTemp to Execucao")
+    services.import_empenhos(load_everything)
+    print("Moving execucoes from ExecucaoTemp to Execucao")
+    services.update_execucao_table_from_execucao_temp(load_everything)
+    print("Applying From To")
     services.apply_fromto()
-    print("From to applied")
+    print("Execucoes generated")
