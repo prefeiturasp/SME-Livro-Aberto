a2ea0664f90979c0152bb9d2d8dc672b10280e08
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-08-04 13:17:58 -0300

    tests added for remaining dao classes

--- contratos/tests/test_dao_classes.py
+++ contratos/tests/test_dao_classes.py
@@ -2,8 +2,12 @@ from copy import deepcopy
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
-from contratos.dao.dao import EmpenhosSOFCacheDao, ExecucoesContratosDao
-from contratos.models import EmpenhoSOFCache, ExecucaoContrato
+from contratos.dao.dao import (
+    EmpenhosSOFCacheDao, ExecucoesContratosDao, FornecedoresDao,
+    ModalidadesContratosDao, ObjetosContratosDao)
+from contratos.models import (
+    EmpenhoSOFCache, ExecucaoContrato, Fornecedor, ModalidadeContrato,
+    ObjetoContrato)
 from contratos.tests.fixtures import EXECUCAO_CONTRATO_CREATE_DATA
 
 
@@ -35,3 +39,48 @@ class ExecucaoContratoDAOTestCase(TestCase):
         ret = dao.create(**data)
         assert ret == mocked_execucao
         mock_create.assert_called_once_with(**data)
+
+
+class ModalidadeContratoDAOTestCase(TestCase):
+
+    @patch.object(ModalidadeContrato.objects, 'get_or_create')
+    def test_get_or_create(self, mock_get_or_create):
+        dao = ModalidadesContratosDao()
+
+        mocked_return = (Mock(spec=ModalidadeContrato), True)
+        mock_get_or_create.return_value = mocked_return
+
+        data = {"id": 11, "desc": "modalidade"}
+        ret = dao.get_or_create(**data)
+        assert ret == mocked_return
+        mock_get_or_create.assert_called_once_with(**data)
+
+
+class ObjetoContratoDAOTestCase(TestCase):
+
+    @patch.object(ObjetoContrato.objects, 'get_or_create')
+    def test_get_or_create(self, mock_get_or_create):
+        dao = ObjetosContratosDao()
+
+        mocked_return = (Mock(spec=ObjetoContrato), True)
+        mock_get_or_create.return_value = mocked_return
+
+        data = {"id": 11, "desc": "objeto"}
+        ret = dao.get_or_create(**data)
+        assert ret == mocked_return
+        mock_get_or_create.assert_called_once_with(**data)
+
+
+class FornecedorDAOTestCase(TestCase):
+
+    @patch.object(Fornecedor.objects, 'get_or_create')
+    def test_get_or_create(self, mock_get_or_create):
+        dao = FornecedoresDao()
+
+        mocked_return = (Mock(spec=Fornecedor), True)
+        mock_get_or_create.return_value = mocked_return
+
+        data = {"id": 11, "desc": "fornecedor"}
+        ret = dao.get_or_create(**data)
+        assert ret == mocked_return
+        mock_get_or_create.assert_called_once_with(**data)
