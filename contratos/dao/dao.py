bdd6566987cfa9c9563bb05598ee2ed0e62d662a
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-08-04 12:31:25 -0300

    Empenhos, Execucoes, Modalidades, Objetos and Fornecedores dao classes

---
+++ contratos/dao/dao.py
@@ -0,0 +1,48 @@
+from contratos.models import (
+    Fornecedor, EmpenhoSOFCache, ExecucaoContrato, ModalidadeContrato,
+    ObjetoContrato)
+
+
+class EmpenhosSOFCacheDao:
+
+    def __init__(self):
+        self.model = EmpenhoSOFCache
+
+    def get_all(self):
+        pass
+
+
+class ExecucoesContratosDao:
+
+    def __init__(self):
+        self.model = ExecucaoContrato
+
+    def create(self, data):
+        ExecucaoContrato.objects.create(**data)
+
+
+class ModalidadesContratosDao:
+
+    def __init__(self):
+        self.model = ModalidadeContrato
+
+    def get_or_create(self, **data):
+        return ModalidadeContrato.objects.get_or_create(**data)
+
+
+class ObjetosContratosDao:
+
+    def __init__(self):
+        self.model = ObjetoContrato
+
+    def get_or_create(self, data):
+        return ObjetoContrato.objects.get_or_create(**data)
+
+
+class FornecedoresDao:
+
+    def __init__(self):
+        self.model = Fornecedor
+
+    def get_or_create(self, data):
+        return Fornecedor.objects.get_or_create(**data)
