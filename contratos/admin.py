6aadf587592a8095282981e1fda68ad4d548cb0f
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-10-07 00:23:52 -0300

    adding EmpenhoSOF models to admin

--- contratos/admin.py
+++ contratos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import CategoriaContrato, CategoriaContratoFromTo, CategoriaContratoFromToSpreadsheet
+from .models import (
+    CategoriaContrato, CategoriaContratoFromTo,
+    CategoriaContratoFromToSpreadsheet, EmpenhoSOFCache, EmpenhoSOFCacheTemp,
+    EmpenhoSOFFailedAPIRequest)
 
 
 @admin.register(CategoriaContratoFromTo)
@@ -18,3 +21,20 @@ class CategoriaContratoAdmin(admin.ModelAdmin):
     ordering = ('desc',)
     list_display = ('id', 'name', 'slug', 'desc')
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(EmpenhoSOFCache)
+class EmpenhoSOFCacheAdmin(admin.ModelAdmin):
+    list_display = ('indexer', 'codContrato', 'anoExercicioContrato',
+                    'anoEmpenho')
+
+
+@admin.register(EmpenhoSOFCacheTemp)
+class EmpenhoSOFCacheTempAdmin(admin.ModelAdmin):
+    list_display = ('indexer', 'codContrato', 'anoExercicioContrato',
+                    'anoEmpenho')
+
+
+@admin.register(EmpenhoSOFFailedAPIRequest)
+class EmpenhoSOFFailedAPIRequestAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'error_code', 'created_at')
