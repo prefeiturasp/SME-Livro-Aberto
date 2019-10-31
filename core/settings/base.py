b298f8cf0239a56afaa99d542a3469ff946e9cc7
Israel Teixeira <israelst@gmail.com>
2019-10-31 19:11:32 -0300

    Install regionalizacao app

--- core/settings/base.py
+++ core/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'mosaico',
     'geologia',
     'contratos',
+    'regionalizacao',
     'from_to_handler.apps.FromToHandlerConfig',
     'budget_execution.apps.BudgetExecutionConfig',
 ]
