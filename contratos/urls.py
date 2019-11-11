041157c12cce470688c4d3f2edb8640d99505c1d
Cleiton Marques <cleiton.marques@fgv.br>
2019-11-11 14:49:13 -0300

    New rotes to pages saiba-mais

--- contratos/urls.py
+++ contratos/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('sobre/', TemplateView.as_view(template_name='contratos/sobre.html'),
          name='sobre'),
     path('saiba-mais/', TemplateView.as_view(template_name='contratos/saiba_mais.html'), name='saiba_mais'),
+    path('metodologia', TemplateView.as_view(template_name='contratos/metodologia.html'), name='metodologia'),
+    path('tutorial/', TemplateView.as_view(template_name='geologia/tutorial.html'), name='tutorial'),
+
 ]
