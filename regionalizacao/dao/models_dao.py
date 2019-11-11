d6683151f66d37751132bd8b79d05a24b16821c2
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-11-11 09:45:23 -0300

    extracting DistritoZonaFromToDaoSpreadsheet

--- regionalizacao/dao/models_dao.py
+++ regionalizacao/dao/models_dao.py
@@ -2,7 +2,10 @@ from django.db import IntegrityError, transaction
 
 from openpyxl import load_workbook
 
-from regionalizacao.models import PtrfFromTo
+from regionalizacao.models import (
+    DistritoZonaFromTo,
+    PtrfFromTo,
+)
 
 
 class PtrfFromToDao:
@@ -42,4 +45,36 @@ class PtrfFromToDao:
 
 
 class DistritoZonaFromToDao:
-    pass
+    def extract_spreadsheet(self, sheet):
+        filepath = sheet.spreadsheet.path
+        wb = load_workbook(filepath)
+        ws = wb.worksheets[0]
+
+        row = 2
+        row_is_valid = True
+        added = []
+        not_added = []
+        while row_is_valid:
+            coddist = ws['a' + str(row)].value
+            if not coddist:
+                row_is_valid = False
+                continue
+            zona = ws['c' + str(row)].value
+
+            ft = DistritoZonaFromTo()
+            ft.coddist = coddist
+            ft.zona = zona
+            try:
+                with transaction.atomic():
+                    ft.save()
+                added.append(ft.coddist)
+            except IntegrityError:
+                not_added.append(ft.coddist)
+
+            row += 1
+
+        sheet.added_fromtos = added
+        sheet.not_added_fromtos = not_added
+        sheet.extracted = True
+        sheet.save()
+        return added, not_added
--- regionalizacao/tests/test_dao_models.py
+++ regionalizacao/tests/test_dao_models.py
@@ -8,6 +8,8 @@ from model_mommy import mommy
 from regionalizacao.models import (
     PtrfFromTo,
     PtrfFromToSpreadsheet,
+    DistritoZonaFromTo,
+    DistritoZonaFromToSpreadsheet,
 )
 
 
@@ -42,5 +44,40 @@ class TestPtrfFromToDao:
 
         sheet.refresh_from_db()
         assert sheet.extracted is True
-        assert list(map(str, codescs)) == sheet.added_fromtos
+        assert codescs == sheet.added_fromtos
+        assert [] == sheet.not_added_fromtos
+
+
+class TestDistritoZonaFromToDao:
+
+    @pytest.fixture()
+    def file_fixture(self, db):
+        filepath = os.path.join(
+            os.path.dirname(__file__),
+            'data/test_DistritoZonaFromToSpreadsheet.xlsx')
+        with open(filepath, 'rb') as f:
+            yield f
+
+        for ssheet_obj in DistritoZonaFromToSpreadsheet.objects.all():
+            ssheet_obj.spreadsheet.delete()
+
+    def test_extract_spreadsheet(self, file_fixture):
+        sheet = mommy.make(
+            DistritoZonaFromToSpreadsheet, spreadsheet=File(file_fixture))
+        # data is extracted on save
+
+        fts = DistritoZonaFromTo.objects.all().order_by('id')
+        assert 2 == len(fts)
+
+        coddists = [1, 2]
+
+        assert fts[0].coddist == coddists[0]
+        assert fts[0].zona == 'LESTE'
+
+        assert fts[1].coddist == coddists[1]
+        assert fts[1].zona == 'OESTE'
+
+        sheet.refresh_from_db()
+        assert sheet.extracted is True
+        assert coddists == sheet.added_fromtos
         assert [] == sheet.not_added_fromtos
