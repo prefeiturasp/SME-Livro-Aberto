6e0a4f67dee2eb57f5ac638db0022c51e6328fde
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-11-29 19:42:59 -0300

    models Recurso, Subgrupo and Grupo created

--- regionalizacao/models.py
+++ regionalizacao/models.py
@@ -12,6 +12,33 @@ class ExecucaoEscola(models.Model):
                 f'{self.escola.nomesc}')
 
 
+class Recurso(models.Model):
+    execucao = models.ForeignKey('ExecucaoEscola', on_delete=models.CASCADE,
+                                 related_name='recursos')
+    subgrupo = models.ForeignKey('Subgrupo', on_delete=models.CASCADE)
+    cost = models.FloatField()
+    label = models.CharField(max_length=150, null=True, blank=True)
+    amount = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.subgrupo} - {self.cost}'
+
+
+class Subgrupo(models.Model):
+    grupo = models.ForeignKey('Grupo', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Grupo(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Escola(models.Model):
     REDES = (
         ('DIR', 'Rede direta SME'),
