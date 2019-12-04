6ae259e5e328ab3c3327bbf244e59a0898ac64fd
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-12-04 11:46:03 -0300

    HomeView returning zonas data

--- regionalizacao/tests/test_views.py
+++ regionalizacao/tests/test_views.py
@@ -1,6 +1,11 @@
-from django.urls import reverse
+from datetime import date
 
+from django.urls import reverse
+from model_mommy import mommy
 from rest_framework.test import APITestCase
+
+from regionalizacao.models import (
+    Escola, EscolaInfo, TipoEscola, Distrito)
 
 
 class TestHomeView(APITestCase):
@@ -12,6 +17,51 @@ class TestHomeView(APITestCase):
     def test_render_correct_template(self):
         response = self.get()
         self.assertTemplateUsed(response, 'regionalizacao/home.html')
+
+    def test_returns_city_data(self):
+        year = date.today().year
+        escola1 = mommy.make(Escola, codesc='01')
+        escola2 = mommy.make(Escola, codesc='02')
+        escola3 = mommy.make(Escola, codesc='03')
+
+        distrito_s = mommy.make(Distrito, zona='Sul')
+        distrito_n = mommy.make(Distrito, zona='Norte')
+        tipo_i = mommy.make(TipoEscola, etapa='Ensino Infantil')
+        tipo_f = mommy.make(TipoEscola, etapa='Ensino Fundamental')
+
+        mommy.make(EscolaInfo, escola=escola1, distrito=distrito_s,
+                   recursos={'total': 100}, tipoesc=tipo_i, year=year,
+                   rede='DIR')
+        mommy.make(EscolaInfo, escola=escola2, distrito=distrito_s,
+                   recursos={'total': 200}, tipoesc=tipo_f, year=year,
+                   rede='DIR')
+        mommy.make(EscolaInfo, escola=escola3, distrito=distrito_n,
+                   recursos={'total': 55}, tipoesc=tipo_i, year=year,
+                   rede='DIR')
+
+        expected = {
+            'total': 355,
+            'zonas': [
+                {'name': 'Sul', 'total': 300},
+                {'name': 'Norte', 'total': 55},
+            ],
+            'etapas': [
+                {
+                    'name': 'Ensino Infantil',
+                    'unidades': 2,
+                    'total': 155,
+                },
+                {
+                    'name': 'Ensino Fundamental',
+                    'unidades': 1,
+                    'total': 200,
+                }
+            ]
+        }
+
+        response = self.get()
+
+        assert expected == response.data
 
 
 class TestSaibaMaisView(APITestCase):
--- regionalizacao/views.py
+++ regionalizacao/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework.response import Response
+
+from regionalizacao.models import EscolaInfo
+from regionalizacao.serializers import CitySerializer
 
 
-class HomeView(TemplateView):
+class HomeView(generics.ListAPIView):
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    queryset = EscolaInfo.objects.filter(recursos__isnull=False)
+    serializer_class = CitySerializer
     template_name = 'regionalizacao/home.html'
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_queryset())
+        return Response(serializer.data)
