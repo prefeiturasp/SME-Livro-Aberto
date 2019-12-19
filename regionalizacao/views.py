c887113fc47de023d089e4a864c1a60dd9bfbe98
Cleiton <cleiton.marques@fgv.br>
2019-12-19 11:29:35 -0300

    Textos ajuste pt5

--- regionalizacao/views.py
+++ regionalizacao/views.py
@@ -13,7 +13,7 @@ from regionalizacao.serializers import PlacesSerializer
 class EscolaInfoFilter(filters.FilterSet):
     LOCALIDADE_CHOICES = (
         ('zona', 'Zona'),
-        ('dre', 'Diretoria Regional de Ensino'),
+        ('dre', 'Diretoria Regional de Educação'),
     )
 
     zona = filters.CharFilter(field_name='distrito__zona')
