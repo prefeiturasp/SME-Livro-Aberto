10fe68e7b8f030895fe022a4e6bdd3f301aad154
Diego Moreira Guimarães <diegodukao@gmail.com>
2019-12-20 09:37:27 -0300

    removing unused code

--- regionalizacao/dao/eol_api_dao.py
+++ regionalizacao/dao/eol_api_dao.py
@@ -3,47 +3,7 @@ import requests
 from datetime import date
 
 from regionalizacao.constants import EOL_API_URL
-from regionalizacao.dao.models_dao import DreDao, EscolaDao, TipoEscolaDao
-
-
-def update_dre_table():
-    dre_dao = DreDao()
-    url = f'{EOL_API_URL}diretorias/'
-
-    response = requests.get(url)
-    results = response['results']
-
-    created_count = 0
-    updated_count = 0
-    for dre_dict in results:
-        _, created = dre_dao.update_or_create(
-            code=dre_dict['dre'],
-            name=dre_dict['diretoria'],
-        )
-        if created:
-            created_count += 1
-        else:
-            updated_count += 1
-
-    return created_count, updated_count
-
-
-def update_tipo_escola_table():
-    tipo_dao = TipoEscolaDao()
-    url = f'{EOL_API_URL}tipo_escola/'
-
-    response = requests.get(url)
-    results = response['results']
-
-    created_count = 0
-    for tipo_dict in results:
-        _, created = tipo_dao.get_or_create(
-            code=tipo_dict['tipoesc'],
-        )
-        if created:
-            created_count += 1
-
-    return created_count
+from regionalizacao.dao.models_dao import EscolaDao
 
 
 def update_escola_table(years):
