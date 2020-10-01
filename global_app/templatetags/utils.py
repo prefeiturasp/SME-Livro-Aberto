3e372fdae6f01c209c6675100f4c9f01d9343034
Calvin Rossinhole <calvin.masters@gmail.com>
2020-10-01 11:03:20 -0300

    fix filtro merge_dres para comportar unidades

--- global_app/templatetags/utils.py
+++ global_app/templatetags/utils.py
@@ -24,17 +24,22 @@ def split(value, sep=None):
 def merge_dres(locations):
     if 'DRE' in locations[0]['name']:
         total_dre_ipiranga = 0.0
+        total_unidades_dre_ipiranga = 0
         total_dre_pirituba_jaragua = 0.0
+        total_unidades_dre_pirituba_jaragua = 0
         locations_to_remove = []
         for idx, location in enumerate(locations):
             if 'IPIRANGA' in location['name']:
                 total_dre_ipiranga += location['total']
+                total_unidades_dre_ipiranga += location['unidades']
                 locations_to_remove.append(location)
             elif 'PIRITUBA/JARAGUA' in location['name']:
                 total_dre_pirituba_jaragua += location['total']
+                total_unidades_dre_pirituba_jaragua += location['unidades']
                 locations_to_remove.append(location)
-        locations.append({'name': 'DRE IPIRANGA', 'total': total_dre_ipiranga})
-        locations.append({'name': 'DRE PIRITUBA/JARAGUA', 'total': total_dre_pirituba_jaragua})
+        locations.append({'name': 'DRE IPIRANGA', 'total': total_dre_ipiranga, 'unidades': total_unidades_dre_ipiranga})
+        locations.append({'name': 'DRE PIRITUBA/JARAGUA', 'total': total_dre_pirituba_jaragua,
+                          'unidades': total_unidades_dre_pirituba_jaragua})
         for location in locations_to_remove:
             locations.pop(locations.index(location))
     return locations
